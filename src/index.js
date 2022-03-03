f7e0813dcc31f7ab25744b438596781d962cb426
bryansource <bescobar34@yahoo.com>
2022-03-03 00:50:32 -0600

    Pass data through props

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ class Square extends React.Component {
     render() {
       return (
         <button className="square">
-          {/* TODO */}
+          {this.props.value}
         </button>
       );
     }
@@ -14,33 +14,33 @@ class Square extends React.Component {
   
 class Board extends React.Component {
 renderSquare(i) {
-    return <Square />;
+    return <Square value = {i}/>;
 }
 
-render() {
-    const status = 'Next player: X';
+    render() {
+        const status = 'Next player: X';
 
-    return (
-    <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-        {this.renderSquare(0)}
-        {this.renderSquare(1)}
-        {this.renderSquare(2)}
+        return (
+        <div>
+            <div className="status">{status}</div>
+            <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+            </div>
+            <div className="board-row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+            </div>
+            <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+            </div>
         </div>
-        <div className="board-row">
-        {this.renderSquare(3)}
-        {this.renderSquare(4)}
-        {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-        {this.renderSquare(6)}
-        {this.renderSquare(7)}
-        {this.renderSquare(8)}
-        </div>
-    </div>
-    );
-}
+        );
+    }
 }
 
 class Game extends React.Component {
